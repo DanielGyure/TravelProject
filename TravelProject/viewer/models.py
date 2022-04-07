d764ffec8f15ab504efb33d2da69623f5fd6b796
DanielGyure <91905128+DanielGyure@users.noreply.github.com>
2022-04-07 16:27:27 +0300

    Added model: Country

--- TravelProject/viewer/models.py
+++ TravelProject/viewer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name

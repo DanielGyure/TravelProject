b9841bbd61ccc65e0db2045af9968354453acbd9
DanielGyure <91905128+DanielGyure@users.noreply.github.com>
2022-04-07 17:02:27 +0300

    Added model: City

--- TravelProject/viewer/models.py
+++ TravelProject/viewer/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Country(models.Model):
     name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Hotel(models.Model):
     name = models.CharField(max_length=100)

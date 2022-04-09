b06baf7bc6e9b51dfd4c1619e4659a2e2dd4803f
DanielGyure <91905128+DanielGyure@users.noreply.github.com>
2022-04-09 12:57:12 +0300

    Added country code

--- TravelProject/viewer/models.py
+++ TravelProject/viewer/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Country(models.Model):
     name = models.CharField(max_length=10)
+    code = models.CharField(max_length=3, null=True, blank=True)
 
     def __str__(self):
         return self.name

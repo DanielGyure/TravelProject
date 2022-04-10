4c5a1811e729699aea947de860e756bcf8e68688
evelinpt <peetevelin@gmail.com>
2022-04-10 10:54:43 +0300

    Created Welcome page

--- TravelProject/TravelProject/urls.py
+++ TravelProject/TravelProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from viewer.views import WelcomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', WelcomeView.as_view(), name="welcome"),
 ]
